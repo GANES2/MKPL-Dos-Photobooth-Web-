ba5b37f202f57536e085c0197cc8c621aef6c26d
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2026-06-20 00:37:26 +0700

    Memperbaiki file tes

--- src/test/java/com/kampus/tugas_ci_cd/TugasCiCdApplicationTests.java
+++ src/test/java/com/kampus/tugas_ci_cd/TugasCiCdApplicationTests.java
@@ -1,15 +1,15 @@
 package com.kampus.tugas_ci_cd;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TugasCiCdApplicationTests {
 
-    @Test
-    void contextLoads() {
-        assertTrue(false, "Sengaja digagalkan untuk demo testing");
-    }
+	@Test
+	void contextLoads() {
+	}
+
 
 }
